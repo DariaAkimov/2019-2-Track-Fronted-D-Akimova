0db06d4c604811871717583c8c1e1ab941a3a245
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-10 15:49:40 +0000

    Deploy DariaAkimov/2019-2-Track-Frontend-D-Akimova to github.com/DariaAkimov/2019-2-Track-Frontend-D-Akimova.git:gh-pages

---
+++ precache-manifest.e55ce2e99b7a3dc9f1054e1f0300a186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b48e554fd0a5edaf07f99c22680cb2",
+    "url": "DariaAkimov.github.io/2019-2-Track-Frontend-D-Akimova/index.html"
+  },
+  {
+    "revision": "f05f7a111d8da605a122",
+    "url": "DariaAkimov.github.io/2019-2-Track-Frontend-D-Akimova/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "612322a642a5431bff86",
+    "url": "DariaAkimov.github.io/2019-2-Track-Frontend-D-Akimova/static/js/2.8c1aa230.chunk.js"
+  },
+  {
+    "revision": "f05f7a111d8da605a122",
+    "url": "DariaAkimov.github.io/2019-2-Track-Frontend-D-Akimova/static/js/main.c88521ec.chunk.js"
+  },
+  {
+    "revision": "c862e9fb0abff16d54f2",
+    "url": "DariaAkimov.github.io/2019-2-Track-Frontend-D-Akimova/static/js/runtime-main.f0ab01de.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "DariaAkimov.github.io/2019-2-Track-Frontend-D-Akimova/static/media/logo.795fb555.svg"
+  }
+]);
